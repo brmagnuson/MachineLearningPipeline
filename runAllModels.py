dfab66c3ef04dbf7d85b8095afd00f435e583d80
Bonnie Magnuson <brmagnuson@users.noreply.github.com>
2016-01-08 15:14:24 -0800

    Fixed multithreading timer so that time is calculated only after all threads are complete.

--- runAllModels.py
+++ runAllModels.py
@@ -8,22 +8,43 @@ multiThreading = True
 
 # regions = ['CoastMnt', 'IntMnt', 'Xeric']
 regions = ['IntMnt']
-months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+# months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+months = ['jan', 'feb']
 
+startSecond = time.time()
 startTime = time.strftime('%a, %d %b %Y %X')
 
+threads = []
 for region in regions:
     for month in months:
 
         if multiThreading:
+
+            # Build threads
             t = threading.Thread(target=thesisFunctions.runAllModels, args=(month, region, randomSeed))
-            t.start()
+            threads.append(t)
+
         else:
+
+            # Run each pipeline
             print('Running pipeline for %s, %s' % (region, month.capitalize()))
             thesisFunctions.runAllModels(month, region, randomSeed)
             print()
 
+if multiThreading:
+
+    # Start all threads
+    for t in threads:
+        t.start()
+
+    # Wait for all threads to finish before continuing
+    for t in threads:
+        t.join()
+
+endSecond = time.time()
 endTime = time.strftime('%a, %d %b %Y %X')
+totalSeconds = endSecond - startSecond
 
 print('Start time:', startTime)
 print('End time:', endTime)
+print('Total: {} minutes and {} seconds'.format(int(totalSeconds // 60), round(totalSeconds % 60)))
