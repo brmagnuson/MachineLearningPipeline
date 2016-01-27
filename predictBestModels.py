78f105e630892e4794c50ce56e382bb1cff9ba41
Bonnie Magnuson <brmagnuson@users.noreply.github.com>
2016-01-27 12:45:04 -0800

    Previous commit should be: added functionality to output predictions and to loop through prediction process

--- predictBestModels.py
+++ predictBestModels.py
@@ -13,5 +13,6 @@ selectedFeaturesList = ['p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p
                         'p2sum', 'p3sum', 'p6sum', 'PERMAVE', 'RFACT', 'DRAIN_SQKM', 'ELEV_MEAN_M_BASIN_30M',
                         'WD_BASIN']
 
+
 thesisFunctions.processSacPredictions(basePath, region, month, randomSeed, trainFeaturesIndex, trainLabelIndex,
                                       selectedFeaturesList, modelIndex)
