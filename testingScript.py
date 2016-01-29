e78eea92d9c363184878a0bcf0220a4b5ae45e7f
Bonnie Magnuson <brmagnuson@users.noreply.github.com>
2016-01-28 20:49:41 -0800

    Framework for a Sacramento-specific training set

--- testingScript.py
+++ testingScript.py
@@ -18,22 +18,19 @@ import thesisFunctions
 
 
 basePath = 'AllMonthsDryHalf/'
-month = 'apr'
+month = 'feb'
 region = 'IntMnt'
 randomSeed = 47392
-myFeaturesIndex = 6
-myLabelIndex = 5
+trainFeaturesIndex = 6
+trainLabelIndex = 5
 modelIndex = 1
 selectedFeaturesList = ['p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12',
                         't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12',
                         'p2sum', 'p3sum', 'p6sum', 'PERMAVE', 'RFACT', 'DRAIN_SQKM', 'ELEV_MEAN_M_BASIN_30M',
                         'WD_BASIN']
 
-thesisFunctions.prepSacramentoData(month, region, basePath)
-
-# result = thesisFunctions.findModelAndPredict(basePath, month, region, randomSeed, myFeaturesIndex, myLabelIndex,
-#                                     selectedFeaturesList, modelIndex)
-
+thesisFunctions.processSacPredictions(basePath, region, month, randomSeed, trainFeaturesIndex, trainLabelIndex,
+                                                  selectedFeaturesList, modelIndex)
 
 
 
