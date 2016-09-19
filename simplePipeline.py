3098b33337ac6c5726a010dc22d8720fb4020125
Bonnie Magnuson <brmagnuson@users.noreply.github.com>
2016-09-19 09:55:47 -0700

    Splitting thesis work and mlutilities into their own repositories for better organization

--- simplePipeline.py
+++ simplePipeline.py
@@ -9,12 +9,11 @@ startTime = time.strftime('%a, %d %b %Y %X')
 
 # Parameters
 runScaleDatasets = True
-runFeatureEngineering = True
-runEnsembleModels = True
+runFeatureEngineering = False
+runEnsembleModels = False
 multiThreadApplyModels = True
 
 randomSeed = constants.randomSeed
-
 month = 'jul'
 region = 'IntMnt'
 basePath = 'Data/'
@@ -23,12 +22,12 @@ universalTestSetDescription = month.capitalize() + ' ' + region + ' Test'
 scoreOutputFilePath = 'Output/testResults.csv'
 myFeaturesIndex = 6
 myLabelIndex = 5
-
 selectedFeatureList = ['p0', 'p1', 'p2', 'p3', 'p4', 'p5', 'p6', 'p7', 'p8', 'p9', 'p10', 'p11', 'p12',
                        't0', 't1', 't2', 't3', 't4', 't5', 't6', 't7', 't8', 't9', 't10', 't11', 't12',
                        'p2sum', 'p3sum', 'p6sum', 'PERMAVE', 'RFACT', 'DRAIN_SQKM', 'ELEV_MEAN_M_BASIN_30M',
                        'WD_BASIN']
 
+# Run the pipeline
 flowModelResult = thesisFunctions.flowModelPipeline(universalTestSetFileName,
                                                     universalTestSetDescription,
                                                     basePath,
